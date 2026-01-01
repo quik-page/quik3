292831df43021f18bd46b186c19c97034574c005
雨竹upon <98014938+siquan001@users.noreply.github.com>
2026-01-01 14:33:19 +0000

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+    <h1>Hello,World!</h1>
+    <input type="text">
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
